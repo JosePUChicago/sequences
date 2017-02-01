4ba6e70f9dd8caffa8ffc429764ca21fde55fd7c
JosePUChicago <josePUChicago@users.noreply.github.com>
2017-02-01 10:16:57 -0600

    no results

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/all_guesses", { :controller => "application", :action => "guesses"})
+  get("/show_answer", { :controller => "application", :action => "answer"})
+  get("/", { :controller => "application", :action => "guesses"})
 end
